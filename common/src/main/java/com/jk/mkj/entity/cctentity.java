dd53b021e2ab68d2fac8fe1596e0868c87ac4612
libra0879 <2647314845@qq.com>
2020-10-23 21:44:16 +0800

    注é曹骏提交的类

---
+++ common/src/main/java/com/jk/mkj/entity/cctentity.java
@@ -0,0 +1,16 @@
+package com.jk.mkj.entity;
+
+/**
+ * TODO
+ *
+ * @author Chentao
+ * @version 1.0
+ * @date 2020/10/23 21:41
+ */
+public class cctentity {
+
+    private Integer id;
+
+    private  String name;
+
+}
