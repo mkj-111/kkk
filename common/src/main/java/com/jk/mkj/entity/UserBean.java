4ab0eb07ec3148aeb75fd7b10c5ef452cc77ccc6
mkj-111 <1816775072@qq.com>
2020-10-23 20:28:00 +0800

    创建实体类

---
+++ common/src/main/java/com/jk/mkj/entity/UserBean.java
@@ -0,0 +1,29 @@
+package com.jk.mkj.entity;
+
+/**
+ * @author 毛康健
+ * @description
+ * @create 2020/10/23
+ */
+public class UserBean {
+
+    private Integer userId;
+
+    private String userName;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
