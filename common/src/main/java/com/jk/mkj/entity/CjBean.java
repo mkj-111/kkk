eb44f4d73012915078c60621a924f8a97f087249
libra0879 <2647314845@qq.com>
2020-10-23 21:33:37 +0800

    提交曹骏类

---
+++ common/src/main/java/com/jk/mkj/entity/CjBean.java
@@ -0,0 +1,17 @@
+package com.jk.mkj.entity;
+
+/**
+ * TODO
+ *
+ * @author Chentao
+ * @version 1.0
+ * @date 2020/10/23 21:32
+ */
+public class CjBean {
+
+    private Integer cid;
+
+    private String cname;
+
+
+}
